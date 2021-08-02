7f495b125fedb0534cd2bb9b68e6819097fc230d
marlen.amezquita <marlen.amezquita@ceiba.com.co>
2021-08-01 23:09:35 -0500

    implementación scenario

---
+++ src/main/java/compraLibros/Pedido.java
@@ -0,0 +1,15 @@
+package compraLibros;
+
+public class Pedido {
+    public Pedido(String tipoCliente) {
+
+    }
+
+    public void agregarProducto(Integer cantidad, String producto) {
+
+    }
+
+    public int calcularCostoEnvio() {
+        return 0;
+    }
+}
--- src/test/java/steps/EntregaGratuitaSteps.java
+++ src/test/java/steps/EntregaGratuitaSteps.java
@@ -1,23 +1,28 @@
 package steps;
 
+import compraLibros.Pedido;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 public class EntregaGratuitaSteps {
+    private Pedido pedido;
+    private int costoEnvio;
 
     @Given("^el comprador es un cliente (.*)$")
     public void indicarTipoClienteQueRealizaElPedido(String tipoCliente) {
-
+        pedido = new Pedido(tipoCliente);
     }
 
     @When("^realiza la compra de (.*) (.*)$")
-    public void agregaProductosAlPedido(Integer cantidad, String producto) {
-
+    public void realizarLaCompraDeLibros(Integer cantidad, String producto) {
+        pedido.agregarProducto(cantidad, producto);
+        costoEnvio = pedido.calcularCostoEnvio();
     }
 
     @Then("^debe pagar (.*)$")
     public void debePagar(int valorPagar) {
-
+        Assert.assertEquals(valorPagar, costoEnvio);
     }
 }
